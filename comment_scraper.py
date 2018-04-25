c5eeb47e1f3fdc94fa6f83228ca8e95d903c6f65
Pio Calderon <piocalderon@gmail.com>
2018-04-25 16:36:52 +0800

    fixed comment scraper & added draft

--- comment_scraper.py
+++ comment_scraper.py
@@ -3,65 +3,63 @@ import time
 import pandas as pd
 import random
 
-token = '' #FB Graph API access token
+running = pd.DataFrame()
 
-def return_request_url(site):
-    return '{}?fields=posts.limit(10){comments{message,created_time,comments.limit(5)},message,created_time}'.format(site)
+site = 'homecreditph'
+owner = 'Home Credit'
 
+token = 'EAACmU0a4eqoBANu3ba3JUpFFi6r1FeKZCD1HfR5s9Wy9VETJ7k3ScUXZB9imgRhczzIJVh3m20OmjaNPxaTLPZAyH0AkR6iZB3QQO6zFDTYx0I1I0OjumpLMiBxlOFjdn8gRhlJ9eoeo5xUFbTj2nzTkLxppE5cZD' #FB Graph API access token
+req = site + '?fields=posts.limit(10){comments{message,created_time,comments.limit(5)},message,created_time}'
+
+def process_dict(row):
     if type(row) != float:
         for com in row['data']:
             if 'from' in com.keys():
-                if com['from']['name'] == owner:
+                if com['from']['name'] == 'GCash':
                     return com['message']
     else:
         return ''
 
 lim_count = 0
 
-# for example, home credit
-site = 'homecreditph'
-owner = 'Home Credit'
-
-post_resp = requests.get('https://graph.facebook.com/v2.11/' + return_request_url(site), {'access_token': token})    
-
-# running will be a df with post message, comment, and first-level owner reply for each comment and post on the fb page 
-running = pd.DataFrame()
-
+post_resp = requests.get('https://graph.facebook.com/v2.11/' + req, {'access_token': token})    
 while True:
+#     print("lim", lim_count)
     if lim_count == 0:
         rjson = post_resp.json()['posts']
     else:
         rjson = post_resp.json()
-    for i in range(len(rjson['data'])): # loop over posts
+    for i in range(len(rjson['data'])):
         print('Looking at post {}...'.format(10*lim_count+i))
         if 'message' in rjson['data'][i].keys():
             print('   Body {}: {}'.format(i, rjson['data'][i]['message'][:70]))
         first = True
-        try: # put comments in a df
+        try:
             comments_for_post = pd.DataFrame(rjson['data'][i]['comments']['data'])
         except:
             comments_for_post = pd.DataFrame()
         while True:
             if first == True:
                 try:
-                    # go to next page of comments
                     comments_url = rjson['data'][i]['comments']['paging']['next']
                 except:
                     break
-            # put a buffer to go over rate limit
+#                     print(comments_url)
             time.sleep(random.randint(2,5))
             r = requests.get(comments_url)
             comments_data = r.json()['data']
+#                 print('before', comments_for_post.shape)
             comments_for_post = pd.concat([comments_for_post, pd.DataFrame(comments_data)], ignore_index=True)
+#                 print('after', comments_for_post.shape)
             try:
-                # go to next page of comments
                 comments_url = r.json()['paging']['next']
+#                     print(comments_url)
                 first = False
             except KeyError:
                 break
         print('   Number of comments: {}'.format(comments_for_post.shape[0]))
         if 'comments' in comments_for_post.columns:
-            comments_for_post['reply'] = comments_for_post['comments'].map(lambda x: process_dict(x, owner))
+            comments_for_post['reply'] = comments_for_post['comments'].map(process_dict)
             comments_for_post = comments_for_post.drop(['comments'], axis=1)
         if 'id' in comments_for_post.columns:
             comments_for_post = comments_for_post.drop(['id'], axis=1)
@@ -71,8 +69,27 @@ while True:
             comments_for_post['post'] = ''
         running = pd.concat([running, comments_for_post], ignore_index=True)
     post_url = rjson['paging']['next']
+#         print('POST', post_url)
     post_resp = requests.get(post_url)
-#         if lim_count == 9: # if this is not set, loop over all posts in the page
+#         if lim_count == 9: # if this is not set, loop over all posts forever
 #             break
     lim_count += 1
+
+running.to_csv('{}.csv'.format(site))
+
+
+
+# # to go to lim_count
+# lim_count = 0
+# post_resp = requests.get('https://graph.facebook.com/v2.11/' + req, {'access_token': token})  
+# while lim_count != 47:
+#     print(lim_count)
+#     post_url = rjson['paging']['next']
+#     if lim_count == 0:
+#         rjson = post_resp.json()['posts']
+#     else:
+#         rjson = post_resp.json()
+#     post_url = rjson['paging']['next']
+#     post_resp = requests.get(post_url)
+#     lim_count += 1
 
